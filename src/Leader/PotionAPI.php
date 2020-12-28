dc9dbbc971a158946704607734cfe471ce88be5d
SangHyeon-Dev <sanghyeon302403@gmail.com>
2020-12-28 23:28:37 +0900

    Initial File Upload

---
+++ src/Leader/PotionAPI.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Leader;
+
+use pocketmine\entity\Effect;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\entity\EffectInstance;
+use pocketmine\network\mcpe\protocol\OnScreenTextureAnimationPacket;
+use pocketmine\Server;
+
+class PotionAPI extends PluginBase implements Listener {
+
+  /** @var PotionAPI */
+  private static $instance;
+
+  public static function getInstance() : self {
+        return self::$instance;
+    }
+
+  public function onLoad(){
+		self::$instance = $this;
+
+	}
+    public function onEnable() {
+        Server::getInstance()->getPluginManager()->registerEvents($this,$this);
+    }
+
+    public function onEffect(Player $player , int $Type , int $Unit , int $Time , int $Power) {
+        $Packet = new OnScreenTextureAnimationPacket();
+        $Packet->effectId = $Type;
+        $player->dataPacket($Packet);
+        $player->addEffect(new EffectInstance(Effect::getEffect($Type),$Unit * $Time, $Power, true));
+    }
+}
